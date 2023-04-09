b618c9a3ceeb70e30c008c2f70b3bbdce81272e3
Justine <115434763+JustinePile@users.noreply.github.com>
2023-04-09 16:54:54 -0600

    Updated code Changes to be committed: 	modified:   static/js/logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,35 +1,40 @@
 // Store our API endpoint as queryUrl.
-let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_day.geojson";
-
+let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_month.geojson";
 // Perform a GET request to the query URL.
 d3.json(queryUrl).then(function(data) {
   // Once we get a response, send the data.features object to the createFeatures function.
   createFeatures(data.features);
 });
 
-// Set different colors from magnitude
-function getColor(magnitude) {
-  let colors = ["#98ee00", "#d4ee00", "#eecc00", "#ee9c00", "#ea822c", "#ea2c2c"];
-  let thresholds = [0, 1, 2, 3, 4, 5];
-  for (let i = 0; i < thresholds.length; i++) {
-    if (magnitude > thresholds[i]) {
-        return colors[i];
-    }
-  }
-  
-  return colors[0];
-}
-
-function styleInfo(feature) {
+function style(feature) {
   return {
     opacity: 1,
     fillOpacity: 1,
     fillColor: getColor(feature.properties.mag),
-    color: "#000000",
+    color: "#000",
     radius: getRadius(feature.properties.mag),
     stroke: true,
-    weight: 0.5
+    weight: 0.6
   };
+}
+
+// Set different colors from magnitude
+function getColor(magnitude) {
+  let colors = ["#fafa6e",  "#d7f171", "#b5e877", "#95dd7d", "#77d183"];
+  let thresholds = [0, 1, 2, 3, 4, 5];
+  for (let i = 0; i < thresholds.length; i++) {
+    if (magnitude > 0 && magnitude < 1) {
+        return colors[0];
+    } else if (magnitude >= 1 && magnitude < 2) {
+      return colors[1];
+    } else if (magnitude >= 2 && magnitude < 3) {
+      return colors[2];
+    } else if (magnitude >= 3 && magnitude < 4) {
+      return colors[3];
+    } else if (magnitude >= 4) {
+    return colors[4];
+  }
+  }
 }
 
 // Set radius from magnitude
@@ -44,21 +49,21 @@ function createFeatures(earthquakeData) {
 
   // Give each feature a popup that describes the place and time of the earthquake
   function onEachFeature(feature, layer) {
-    layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
+    layer.bindPopup(`<h2>Magnitude: ${feature.properties.mag}</h2><h3>Location: ${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
+  }
+
+  function pointToLayer(feature, latlng) {
+      return L.circleMarker(latlng);
   }
 
   // Create a GeoJSON layer that contains the features array on the earthquakeData object
   let earthquakes = L.geoJson(earthquakeData, {
+    onEachFeature: onEachFeature,
     // Make circles
-    pointToLayer: function(feature, latlng) {
-      return L.circleMarker(latlng);
-    },
+    pointToLayer: pointToLayer,
     // Circle style
-    style: styleInfo,
+    style: style,
     // Popup for each marker
-    onEachFeature: function(feature, layer) {
-      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
-    }
   });
 
   // Send our earthquakes layer to the createMap function.
@@ -93,7 +98,7 @@ function createMap(earthquakes) {
       37.09, -95.71
     ],
     zoom: 5,
-    layers: [street, earthquakes]
+    layers: [street, topo, earthquakes]
   });
 
   // Create a layer control
