f98aac7e47d5265bc11515e3458aa47a1b75b582
Justine <115434763+JustinePile@users.noreply.github.com>
2023-04-09 18:30:08 -0600

    Updated code

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,5 +1,5 @@
 // Store our API endpoint as queryUrl.
-let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_month.geojson";
+let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
 // Perform a GET request to the query URL.
 d3.json(queryUrl).then(function(data) {
   // Once we get a response, send the data.features object to the createFeatures function.
@@ -10,7 +10,7 @@ function style(feature) {
   return {
     opacity: 1,
     fillOpacity: 1,
-    fillColor: getColor(feature.properties.mag),
+    fillColor: getColor(feature.geometry.coordinates[2]),
     color: "#000",
     radius: getRadius(feature.properties.mag),
     stroke: true,
@@ -19,37 +19,36 @@ function style(feature) {
 }
 
 // Set different colors from magnitude
-function getColor(magnitude) {
+function getColor(depth) {
   let colors = ["#fafa6e",  "#d7f171", "#b5e877", "#95dd7d", "#77d183"];
-  let thresholds = [0, 1, 2, 3, 4, 5];
-  for (let i = 0; i < thresholds.length; i++) {
-    if (magnitude > 0 && magnitude < 1) {
+    if (depth < 10) {
         return colors[0];
-    } else if (magnitude >= 1 && magnitude < 2) {
+    } else if (depth >= 10 && depth < 20) {
       return colors[1];
-    } else if (magnitude >= 2 && magnitude < 3) {
+    } else if (depth >= 20 && depth < 30) {
       return colors[2];
-    } else if (magnitude >= 3 && magnitude < 4) {
+    } else if (depth >= 30 && depth < 40) {
       return colors[3];
-    } else if (magnitude >= 4) {
+    } else if (depth >= 40) {
     return colors[4];
-  }
   }
 }
 
 // Set radius from magnitude
 function getRadius(magnitude) {
   if (magnitude === 0) {
-    return 1;
+    return .1;
   }
-  return magnitude * 4;
+  return magnitude * 3.5;
 }
 
 function createFeatures(earthquakeData) {
 
   // Give each feature a popup that describes the place and time of the earthquake
   function onEachFeature(feature, layer) {
-    layer.bindPopup(`<h2>Magnitude: ${feature.properties.mag}</h2><h3>Location: ${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
+    layer.bindPopup(`<h2>Depth (km): ${feature.geometry.coordinates[2]} </h2>
+<h3>Magnitude: ${feature.properties.mag}</h3><h3>Location: 
+${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
   }
 
   function pointToLayer(feature, latlng) {
@@ -73,8 +72,8 @@ function createFeatures(earthquakeData) {
 function createMap(earthquakes) {
 
   // Create the base layers.
-  let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  let stamen = L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/terrain/{z}/{x}/{y}.png', {
+    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>'
   });
 
   let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
@@ -83,8 +82,8 @@ function createMap(earthquakes) {
 
   // Create a baseMaps object.
   let baseMaps = {
-    "Street Map": street,
-    "Topographic Map": topo
+    "Topographic Map": topo,
+    "Stamen Map": stamen
   };
 
   // Create an overlay object to hold our overlay.
@@ -95,15 +94,33 @@ function createMap(earthquakes) {
   // Create our map, giving it the streetmap and earthquakes layers to display on load.
   let myMap = L.map("map", {
     center: [
-      37.09, -95.71
+      51, -120
     ],
-    zoom: 5,
-    layers: [street, topo, earthquakes]
+    zoom: 4,
+    layers: [stamen, topo, earthquakes]
   });
 
   // Create a layer control
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
   }).addTo(myMap);
+
+  // Set up the legend.
+  let legend = L.control({ position: "bottomright" });
+
+  legend.onAdd = function(map) {
+    var div = L.DomUtil.create("div", "legend");
+    div.innerHTML += "<h4>Depth (km)</h4>";
+    div.innerHTML += '<i style="background: #fafa6e"></i><span>&lt; 10</span><br>';
+    div.innerHTML += '<i style="background: #d7f171"></i><span>10 - 20</span><br>';
+    div.innerHTML += '<i style="background: #b5e877"></i><span>20 - 30</span><br>';
+    div.innerHTML += '<i style="background: #95dd7d"></i><span>30 - 40</span><br>';
+    div.innerHTML += '<i style="background: #77d183"></i><span>&gt; 40<span><br>';
+      
+    return div;
+  };
+
+  // Adding the legend to the map
+  legend.addTo(myMap);
 
 };
